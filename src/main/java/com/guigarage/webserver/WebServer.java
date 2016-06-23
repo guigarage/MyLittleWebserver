c54542c68bc22c801376721017fda84ef7275a3c
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-06-23 10:19:55 +0200

    Wait

--- src/main/java/com/guigarage/webserver/WebServer.java
+++ src/main/java/com/guigarage/webserver/WebServer.java
@@ -1,14 +1,19 @@
 package com.guigarage.webserver;
 
+import java.time.LocalTime;
+
 /**
  * Created by hendrikebbers on 23.06.16.
  */
 public class WebServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         while (true) {
             HttpRequest request = getNextRequest();
-            request.sendResponse("Hello from Server!");
+
+            Thread.sleep(5000);
+
+            request.sendResponse("Hello from Server! Time: " + LocalTime.now());
         }
     }
 
